23762f30b1ac317cf3db9c2773ba9d6210fed229
Jhonny L. Alcius <louis.jhonny@gmail.com>
2020-07-17 13:29:53 -0400

    Add Schedule feature

--- functions.js
+++ functions.js
@@ -7,13 +7,25 @@ const ads = new Set();
 const adsDetail = new Set();
 const positions = new Set();
 const extractor = require('libphonenumber-js');
+const jsonstr = [];
 
+/**
+ * 
+ * @param {*} url 
+ */
 const loadSiteData = async (url) => {
     const result = await axios.get(url);
     return cheerio.load(result.data);
 };
 
-const saveAdsData = async (url, data) => {
+
+/**
+ * 
+ * @param {*} url 
+ * @param {*} data 
+ * @param {*} dataset 
+ */
+const saveAdsData = async (url, data, dataset) => {
     const $ = await loadSiteData(url);
     $('[class^=crumbItem]').each((index, element)=>{
         switch(index){
@@ -42,11 +54,28 @@ const saveAdsData = async (url, data) => {
     if(phoneData[1] !== undefined && data.phone1 != phoneData[1].phone){
         data.phone2 = phoneData[1].phone;
     }
-    
     adsDetail.add(data);
-    console.log(adsDetail);
+
+    if(adsDetail.size == dataset.size){
+       return adsDetail;
+    }
 }
 
+
+/**
+ * 
+ */
+const getAdsv2 = async () => {
+    const $ = await loadSiteData(params.config.startUrl);
+    $('.regular-ad').each((index, element)=>{
+        jsonstr.push({'title':$(element).find('.title').text(), 'link':$(element).find('.title').attr('href'), 'id':$(element).attr('data-listing-id')});
+    });    
+}
+
+
+/**
+ * 
+ */
 const getAds = async () => {
     const $ = await loadSiteData(params.config.startUrl);
     $('.regular-ad').each((index, element)=>{
@@ -54,28 +83,90 @@ const getAds = async () => {
     });    
 }
 
+
+/**
+ * 
+ */
 const getAdsDetail = async () => {
     await getAds();
-    ads.forEach( (value) =>{
+    ads.forEach( (value, index, ads) =>{
         if(!value.link.includes(siteRoot)){
             value.link = siteRoot + value.link;
         }        
-        saveAdsData(value.link, value);
+        
+        let dataset = saveAdsData(value.link, value, ads);
+        dataset.then(function(result){
+            if(result){
+                console.log(result);
+            }
+        })
     });
+    
 }
 
+
+/**
+ * 
+ */
+const getAdsDetailv2 = async () => {
+    await getAdsv2();
+
+    for(var key in jsonstr){
+        if(!jsonstr[key].link.includes(siteRoot)){
+            jsonstr[key].link = siteRoot + jsonstr[key].link;
+        } 
+       const $ = await loadSiteData(jsonstr[key].link);
+
+       $('[class^=crumbItem]').each((index, element)=>{
+            switch(index){
+                case 0:
+                jsonstr[key].province = $(element).text();
+                break;
+
+                case 1:
+                jsonstr[key].city = $(element).text();
+                break;
+            }
+        
+        }); 
+
+        description = $('[class^=descriptionContainer]').find('div[itemprop=description]').text();
+        emails = (extractEmails(description) != null)? extractEmails(description).join(','):'';
+        let phoneData = extractor.findNumbers(description, "CA");
+        jsonstr[key].phone1='';
+        jsonstr[key].phone2='';
+
+        jsonstr[key].email = emails;
+
+        if(phoneData[0] !== undefined){
+            jsonstr[key].phone1 = phoneData[0].phone;
+        }
+
+        if(phoneData[1] !== undefined && jsonstr[key].phone1 != phoneData[1].phone){
+            jsonstr[key].phone2 = phoneData[1].phone;
+        } 
+    }
+    console.log(jsonstr);
+}
+
+
+/**
+ * 
+ * @param {*} text 
+ */
 function extractEmails (text)
 {
     return text.match(/([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9._-]+)/gi);
 }
 
-const saveToJson = (data)=>{
-    var jsonstr = JSON.stringify(data);
-    //fs.writeFile('./ads/skilled-trades.json', JSON.stringify(data), () => { })
-}
-//Pad given value to the left with "0"
+
+/**
+ * 
+ * @param {*} num 
+ */
 function AddZero(num) {
     return (num >= 0 && num < 10) ? "0" + num : num + "";
 }
 
 exports.getAdsDetail = getAdsDetail;
+exports.getAdsDetailv2 = getAdsDetailv2;
--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 const fs = require('fs');
 const fn = require('./functions');
+const schedule = require('node-schedule');
 
-// const category = fn.loadAdsCategories();
-// console.log(category);
+//create a schedule before continue
+var secondes = "";     // (0-59) optional
+var minutes = "10";     // (0-59) required
+var hour = "*";         // (0-23) required
+var day_of_month = "*"; // (1-31) required
+var month = "*";        // (1-12) required
+var day_of_week = "*";  // (0-7) required : 0 or 7 is Sun
 
-(async () => {
-    const allAds = await fn.getAdsDetail();
-})();
+secondes = (secondes != "")? secondes + ' ' : '';
+
+let scrapschedule = minutes +' '+ hour +' '+ day_of_month +' '+ month +' '+ day_of_week;
+console.log(scrapschedule);
+
+var task = schedule.scheduleJob(scrapschedule, async function(){
+    let allAds = await fn.getAdsDetail();
+});
