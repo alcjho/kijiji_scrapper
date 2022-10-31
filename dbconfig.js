dca6e767849d52a9b02df04da15a49f0c07c1b59
ajhonny <louis.jhonny@gmail.com>
2022-10-31 07:10:04 -0400

    add dbconfig* to gitignore

---
+++ dbconfig.js
@@ -0,0 +1,11 @@
+const srv5 = {
+    host: 'soumissionrenovation.ca',
+    user: 'micheljodoin',
+    database: 'srv5',
+    password: 'NPVVTsDdPwf7TTzfWAQj3QNBvsZ478oxuu4M3cB2j7xLDq7HBw',
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+}
+
+exports.srv5 = srv5;
